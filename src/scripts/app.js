47078d45aa871a72b51c1ec3d38b61bee7ca88a7
TardiGab <tardigab@gmail.com>
2025-04-02 15:05:11 +0200

    Fetch PokéAPI and send data in console.log

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,19 @@
 "use strict";
 
+const pokeName = document.querySelector("#pokeName");
+// const pokeValue = pokeName.value;
+const searchButton = document.querySelector("#searchButton");
+const form = document.querySelector("#pokeForm");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const pokeValue = pokeName.value;
+  console.log(pokeValue);
+  fetch(`https://web.mayfly.ovh/proxy/pokeapi.php?endpoint=pokemon/${pokeValue}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+});
